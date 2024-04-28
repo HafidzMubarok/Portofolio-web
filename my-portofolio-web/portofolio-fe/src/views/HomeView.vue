6c3350b2a2bbbc8d5bcfa270c7a522ce98225b5a
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-04-28 14:01:49 +0700

    :lipstick: [Hero] Switch position of button and contact

--- my-portofolio-web/portofolio-fe/src/views/HomeView.vue
+++ my-portofolio-web/portofolio-fe/src/views/HomeView.vue
@@ -4,16 +4,21 @@
 
 <template>
     <section id="home" class="px-12 bg-primary lg:ps-36">
-        <div class="container py-12 lg:py-24">
+        <div class="container py-12 lg:pt-24 lg:pb-12">
             <div class="flex flex-wrap space-y-6">
                 <div class="w-full self-center space-y-6 md:w-auto lg:basis-1/2">
                     <h1 class="block font-bold text-light text-center text-3xl md:text-6xl md:text-start">MUHAMMAD FATHUL HAFIDZ MUBAROK</h1>
                     <h2 class="font-normal text-light text-center md:text-start md:text-2xl">Software Engineer | Web Developer</h2>
                 </div>
                 <div class="lg:basis-1/4 lg:mx-auto lg:pb-4">
-                    <img src="../../public/img/profile-pic-2.jpg" alt="Profile Pic" class="max-w-full mx-auto px-16 md:max-w-96 md:ps-0 lg:px-8">
+                    <img src="../../public/img/profile-pic-2.png" alt="Profile Pic" class="max-w-full mx-auto px-16 md:max-w-96 md:ps-0 lg:px-8">
                 </div>
-                <div class="w-full self-center space-y-6 md:w-auto lg:basis-1/2 ">
+                <div class="w-full self-center space-y-6 md:w-auto lg:basis-1/2">
+                    <div class="text-center py-3 md:text-start">
+                        <a href="#"
+                            class="text-base font-semibold text-light bg-secondary rounded-md py-3 px-8 hover:shadow-lg hover:opacity-80">Explore
+                            <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
                     <div class="space-x-6 text-light text-center md:text-start">
                         <a href="#">
                             <i class="fa-solid fa-envelope fa-xl"></i>
@@ -30,11 +35,6 @@
                         <a href="#">
                             <i class="fa-brands fa-instagram fa-xl"></i>
                         </a>
-                    </div>
-                    <div class="text-center py-3 md:text-start">
-                        <a href="#"
-                            class="text-base font-semibold text-light bg-secondary rounded-md py-3 px-8 hover:shadow-lg hover:opacity-80">Explore
-                            <i class="fa-solid fa-arrow-right"></i></a>
                     </div>
                 </div>
             </div>
