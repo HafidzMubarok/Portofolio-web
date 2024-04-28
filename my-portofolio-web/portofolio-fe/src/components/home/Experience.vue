486f68ce676425ac2e2c5395d86ce66a07983480
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-04-28 20:56:27 +0700

    :lipstick: [Experience] Change text description of work history

--- my-portofolio-web/portofolio-fe/src/components/home/Experience.vue
+++ my-portofolio-web/portofolio-fe/src/components/home/Experience.vue
@@ -2,18 +2,19 @@
     <section id="experience" class="bg-light">
         <div class="px-10 py-8 text-center space-y-4">
             <h1 class="text-primary font-bold text-2xl">My Experience and Job History</h1>
-            <p class="text-primary text-base">Job History</p>
+            <p class="text-primary text-base md:text-lg">Job History</p>
         </div>
-        <div class="flex flex-col px-6 pb-8 space-y-4 md:flex-row md:flex-wrap md:space-x-4 md:space-y-0 md:justify-center">
-            <div class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
+        <div
+            class="flex flex-col px-6 pb-8 space-y-4 md:flex-row md:flex-wrap md:space-x-4 md:space-y-0 md:justify-center">
+            <div
+                class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
                 <h5 class="mb-2 text-xl font-bold leading-tight">PT Lapi Divusi</h5>
                 <p class="text-base">Web Developer</p>
                 <p class="font-light text-sm">November 2022 - July 2023</p>
-                <p class="mb-4 text-base truncate">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error odit aspernatur incidunt ullam earum
-                    cum
-                    magni assumenda quasi, iusto nesciunt quidem eum doloremque architecto tempore ab temporibus, veniam
-                    quaerat impedit?
+                <p class="mb-4 text-base">
+                    Have been working part-time and involved in the development of employee data management web
+                    applications. Working using Laravel framework, Bootstrap, PostgreSQL database, GitLab version
+                    control, and other supporting tools. other supporting tools.
                 </p>
                 <!-- <button type="button"
                     class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 w-max text-xs font-medium uppercase leading-normal 
@@ -24,15 +25,14 @@
                     Show More
                 </button> -->
             </div>
-            <div class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
+            <div
+                class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
                 <h5 class="mb-2 text-xl font-bold leading-tight">PT Lapi Divusi</h5>
                 <p class="text-base">Web Developer</p>
                 <p class="font-light text-sm">November 2022 - July 2023</p>
-                <p class="mb-4 text-base truncate">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error odit aspernatur incidunt ullam earum
-                    cum
-                    magni assumenda quasi, iusto nesciunt quidem eum doloremque architecto tempore ab temporibus, veniam
-                    quaerat impedit?
+                <p class="mb-4 text-base">
+                    Involved in the development of employee data management web applications, such as the creation of
+                    features based on CRUD based features with Laravel and PostgreSQL technology.
                 </p>
             </div>
         </div>
