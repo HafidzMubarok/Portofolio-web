8ad0c59d7e5619417b3927c1b0da0770c0edc848
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-04-28 20:47:07 +0700

    :lipstick: [Experience] Add job history content

---
+++ my-portofolio-web/portofolio-fe/src/components/home/Experience.vue
@@ -0,0 +1,46 @@
+<template>
+    <section id="experience" class="bg-light">
+        <div class="px-10 py-8 text-center space-y-4">
+            <h1 class="text-primary font-bold text-2xl">My Experience and Job History</h1>
+            <p class="text-primary text-base">Job History</p>
+        </div>
+        <div class="flex flex-col px-6 pb-8 space-y-4 md:flex-row md:flex-wrap md:space-x-4 md:space-y-0 md:justify-center">
+            <div class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
+                <h5 class="mb-2 text-xl font-bold leading-tight">PT Lapi Divusi</h5>
+                <p class="text-base">Web Developer</p>
+                <p class="font-light text-sm">November 2022 - July 2023</p>
+                <p class="mb-4 text-base truncate">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error odit aspernatur incidunt ullam earum
+                    cum
+                    magni assumenda quasi, iusto nesciunt quidem eum doloremque architecto tempore ab temporibus, veniam
+                    quaerat impedit?
+                </p>
+                <!-- <button type="button"
+                    class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 w-max text-xs font-medium uppercase leading-normal 
+                    text-light shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 
+                    focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 
+                    active:shadow-primary-2"
+                    data-twe-ripple-init data-twe-ripple-color="light">
+                    Show More
+                </button> -->
+            </div>
+            <div class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
+                <h5 class="mb-2 text-xl font-bold leading-tight">PT Lapi Divusi</h5>
+                <p class="text-base">Web Developer</p>
+                <p class="font-light text-sm">November 2022 - July 2023</p>
+                <p class="mb-4 text-base truncate">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error odit aspernatur incidunt ullam earum
+                    cum
+                    magni assumenda quasi, iusto nesciunt quidem eum doloremque architecto tempore ab temporibus, veniam
+                    quaerat impedit?
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
--- my-portofolio-web/portofolio-fe/src/views/HomeView.vue
+++ my-portofolio-web/portofolio-fe/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
     import Hero from "../components/home/Hero.vue";
     import AboutMe from "../components/home/AboutMe.vue";
+    import Experience from "../components/home/Experience.vue";
 </script>
 
 <template>
     <Hero />
     <AboutMe />
+    <Experience />
 </template>
