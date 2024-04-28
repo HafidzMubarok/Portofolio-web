50f5f01da769663ee0e8787a806693e77595732d
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-04-28 21:17:26 +0700

    :lipstick: [Experience & About Me] Adjust text size

--- my-portofolio-web/portofolio-fe/src/components/home/AboutMe.vue
+++ my-portofolio-web/portofolio-fe/src/components/home/AboutMe.vue
@@ -1,8 +1,8 @@
 <template>
     <section id="about-me" class="bg-sky-100 lg:px-24">
         <div class="px-10 py-8 space-y-4">
-            <h1 class="text-primary font-bold text-2xl">About Me</h1>
-            <p>Fresh graduate as a software engineer with experience in the field of
+            <h1 class="text-primary font-bold text-2xl lg:text-3xl">About Me</h1>
+            <p class="lg:text-lg">Fresh graduate as a software engineer with experience in the field of
                 computer engineering and informatics especially web development as a backend and frontend developer.
                 Innovative and creative in in dealing with problems.</p>
         </div>
--- my-portofolio-web/portofolio-fe/src/components/home/Experience.vue
+++ my-portofolio-web/portofolio-fe/src/components/home/Experience.vue
@@ -1,8 +1,8 @@
 <template>
     <section id="experience" class="bg-light">
         <div class="px-10 py-8 text-center space-y-4">
-            <h1 class="text-primary font-bold text-2xl">My Experience and Job History</h1>
-            <p class="text-primary text-base md:text-lg">Job History</p>
+            <h1 class="text-primary font-bold text-2xl lg:text-3xl">My Experience and Job History</h1>
+            <p class="text-primary text-base md:text-lg lg:text-xl">Job History</p>
         </div>
         <div
             class="flex flex-col px-6 pb-8 space-y-4 md:flex-row md:flex-wrap md:space-x-4 md:space-y-0 md:justify-center">
@@ -11,36 +11,56 @@
                 <h5 class="mb-2 text-xl font-bold leading-tight">PT Lapi Divusi</h5>
                 <p class="text-base">Web Developer</p>
                 <p class="font-light text-sm">November 2022 - July 2023</p>
-                <p class="mb-4 text-base">
+                <p class="mb-4 text-base truncate">
                     Have been working part-time and involved in the development of employee data management web
                     applications. Working using Laravel framework, Bootstrap, PostgreSQL database, GitLab version
                     control, and other supporting tools. other supporting tools.
                 </p>
-                <!-- <button type="button"
+                <button type="button"
                     class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 w-max text-xs font-medium uppercase leading-normal 
                     text-light shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 
                     focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 
                     active:shadow-primary-2"
                     data-twe-ripple-init data-twe-ripple-color="light">
                     Show More
-                </button> -->
+                </button>
             </div>
             <div
                 class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
                 <h5 class="mb-2 text-xl font-bold leading-tight">PT Lapi Divusi</h5>
                 <p class="text-base">Web Developer</p>
                 <p class="font-light text-sm">November 2022 - July 2023</p>
-                <p class="mb-4 text-base">
+                <p class="mb-4 text-base truncate" ref="show">
                     Involved in the development of employee data management web applications, such as the creation of
                     features based on CRUD based features with Laravel and PostgreSQL technology.
                 </p>
+                <button @click="showMore" type="button"
+                    class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 w-max text-xs font-medium uppercase leading-normal 
+                    text-light shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 
+                    focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 
+                    active:shadow-primary-2"
+                    data-twe-ripple-init data-twe-ripple-color="light">
+                    Show More
+                </button>
             </div>
         </div>
     </section>
 </template>
 
 <script setup>
+import { ref } from "vue";
 
+const show = ref(null);
+
+function showMore() {
+    const showClass = show.value.classList;
+
+    if (showClass.contains("truncate")) {
+        showClass.remove("truncate");
+    } else {
+        showClass.add("truncate");
+    }
+}
 </script>
 
 <style lang="scss" scoped></style>
