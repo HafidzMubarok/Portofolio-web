efc974132adc6ae51e06b2311a6e4ed6c71e7365
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-04-28 09:08:33 +0700

    :lipstick: [Nav] Reset all changes of navbar

--- my-portofolio-web/portofolio-fe/src/App.vue
+++ my-portofolio-web/portofolio-fe/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <NavBar />
+    <!-- <NavBar /> -->
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
-import NavBar from "./components/NavBar.vue";
+// import NavBar from "./components/NavBar.vue";
 </script>
 
 <style lang="scss" scoped>
