e8f8e94c28ade76fc58939533f7e64027d4c6a62
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-04-29 10:43:50 +0700

    :lipstick: [About Me] Change background and text color

--- my-portofolio-web/portofolio-fe/src/components/home/AboutMe.vue
+++ my-portofolio-web/portofolio-fe/src/components/home/AboutMe.vue
@@ -1,8 +1,8 @@
 <template>
-    <section id="about-me" class="bg-sky-100 lg:px-24">
+    <section id="about-me" class="bg-primary lg:px-24 lg:py-8">
         <div class="px-10 py-8 space-y-4">
-            <h1 class="text-primary font-bold text-2xl lg:text-3xl">About Me</h1>
-            <p class="lg:text-lg">Fresh graduate as a software engineer with experience in the field of
+            <h1 class="text-light font-bold text-2xl lg:text-3xl">About Me</h1>
+            <p class="lg:text-lg text-light">Fresh graduate as a software engineer with experience in the field of
                 computer engineering and informatics especially web development as a backend and frontend developer.
                 Innovative and creative in in dealing with problems.</p>
         </div>
