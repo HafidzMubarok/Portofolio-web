db1d6cb16bcd0c47d8a41187debaae2855b44998
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-05-01 21:19:53 +0700

    :lipstick: Create component card and implement in section experience

---
+++ my-portofolio-web/portofolio-fe/src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
+        <slot></slot>
+        <button type="button" class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 w-max text-xs font-medium uppercase leading-normal 
+                text-light shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 
+                focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 
+                active:shadow-primary-2" data-twe-ripple-init data-twe-ripple-color="light">
+            Show More
+        </button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
--- my-portofolio-web/portofolio-fe/src/components/home/Experience.vue
+++ my-portofolio-web/portofolio-fe/src/components/home/Experience.vue
@@ -6,8 +6,7 @@
         </div>
         <div
             class="flex flex-col px-6 pb-8 space-y-4 md:flex-row md:flex-wrap md:space-x-4 md:space-y-0 md:justify-center">
-            <div
-                class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
+            <Card>
                 <h5 class="mb-2 text-xl font-bold leading-tight">PT Lapi Divusi</h5>
                 <p class="text-base">Web Developer</p>
                 <p class="font-light text-sm">November 2022 - July 2023</p>
@@ -16,68 +15,44 @@
                     applications. Working using Laravel framework, Bootstrap, PostgreSQL database, GitLab version
                     control, and other supporting tools. other supporting tools.
                 </p>
-                <button type="button" class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 w-max text-xs font-medium uppercase leading-normal 
-                    text-light shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 
-                    focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 
-                    active:shadow-primary-2" data-twe-ripple-init data-twe-ripple-color="light">
-                    Show More
-                </button>
-            </div>
-            <div
-                class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
+            </Card>
+            <Card>
                 <h5 class="mb-2 text-xl font-bold leading-tight">PT Lapi Divusi</h5>
                 <p class="text-base">Web Developer</p>
                 <p class="font-light text-sm">November 2022 - July 2023</p>
-                <p class="mb-4 text-base truncate" ref="show">
+                <p class="mb-4 text-base truncate">
                     Involved in the development of employee data management web applications, such as the creation of
                     features based on CRUD based features with Laravel and PostgreSQL technology.
                 </p>
-                <button @click="showMore" type="button" class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 w-max text-xs font-medium uppercase leading-normal 
-                    text-light shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 
-                    focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 
-                    active:shadow-primary-2" data-twe-ripple-init data-twe-ripple-color="light">
-                    Show More
-                </button>
-            </div>
+            </Card>
         </div>
         <div class="px-10 pb-8 text-center space-y-4">
             <p class="text-primary text-base md:text-lg lg:text-xl">Project</p>
         </div>
         <div
             class="flex flex-col px-6 pb-8 space-y-4 md:flex-row md:flex-wrap md:space-x-4 md:space-y-0 md:justify-center">
-            <div
-                class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
+            <Card>
                 <h5 class="mb-2 text-xl font-bold leading-tight">HRIS</h5>
                 <p class="text-base">Project of PT Lapi Divusi</p>
                 <p class="font-light text-sm">November 2022 - July 2023</p>
                 <p class="mb-4 text-base truncate">
-                    Involved in the development of employee data management web applications, such as the creation of
+                    Involved in the development of employee data management web applications, such as the
+                    creation of
                     features based on CRUD based features with Laravel and PostgreSQL technology.
                 </p>
-                <button type="button" class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 w-max text-xs font-medium uppercase leading-normal 
-                    text-light shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 
-                    focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 
-                    active:shadow-primary-2" data-twe-ripple-init data-twe-ripple-color="light">
-                    Show More
-                </button>
-            </div>
-            <div
-                class="block rounded-lg bg-white p-12 text-surface shadow-secondary-1 text-primary space-y-4 md:max-w-xs lg:max-w-md">
+            </Card>
+            <Card>
                 <h5 class="mb-2 text-xl font-bold leading-tight">SI RUKUN</h5>
                 <p class="text-base">Final Project on Politeknik Negeri Bandung</p>
                 <p class="font-light text-sm">Maret 2023 - Desember 2024</p>
                 <p class="mb-4 text-base truncate">
-                    Completed the development of cover letter and certificate management features, population data
-                    collection features, and user management features using Laravel, PHP, PostgreSQL, and Bootstrap
+                    Completed the development of cover letter and certificate management features, population
+                    data
+                    collection features, and user management features using Laravel, PHP, PostgreSQL, and
+                    Bootstrap
                     development technologies.
                 </p>
-                <button type="button" class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 w-max text-xs font-medium uppercase leading-normal 
-                    text-light shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 
-                    focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 
-                    active:shadow-primary-2" data-twe-ripple-init data-twe-ripple-color="light">
-                    Show More
-                </button>
-            </div>
+            </Card>
             <a href="#" class="p-3 font-semibold text-center bg-primary text-light rounded-lg uppercase md:hidden">Show
                 More
                 Project <i class="fa-solid fa-chevron-down"></i></a>
@@ -85,20 +60,14 @@
     </section>
 </template>
 
-<script setup>
-import { ref } from "vue";
+<script>
+import Card from '../Card.vue';
 
-const show = ref(null);
-
-function showMore() {
-    const showClass = show.value.classList;
-
-    if (showClass.contains("truncate")) {
-        showClass.remove("truncate");
-    } else {
-        showClass.add("truncate");
-    }
-}
+export default {
+    components: {
+        Card,
+    },
+};
 </script>
 
 <style lang="scss" scoped></style>
