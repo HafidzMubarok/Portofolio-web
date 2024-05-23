8a45758a79f46de4bdd456ca06362268ac792c0b
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-05-23 13:55:44 +0700

    📱Show navbar link if resolution is not mobile

--- my-portofolio-web/portofolio-fe/src/components/NavBar.vue
+++ my-portofolio-web/portofolio-fe/src/components/NavBar.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="sticky top-0 w-full bg-gradient-to-r from-sky-600 to-primary">
-    <div class="flex flex-row justify-end text-2xl px-6 py-3">
-      <a href="#" class="text-light">
-        <i class="fa-solid fa-bars"></i>
-      </a>
-    </div>
+    <nav>
+      <div class="flex flex-row justify-end font-semibold text-light text-2xl px-6 py-3 md:justify-center md:text-lg">
+        <a href="#" class="md:hidden">
+          <i class="fa-solid fa-bars"></i>
+        </a>
+        <ul class="sm:hidden md:flex md:flex-row md:space-x-8 ">
+          <li><a href="">About Me</a></li>
+          <li><a href="">History</a></li>
+          <li><a href="">Technology</a></li>
+          <li><a href="">Education</a></li>
+          <li><a href="">Article</a></li>
+        </ul>
+      </div>
+    </nav>
   </div>
 </template>
 
