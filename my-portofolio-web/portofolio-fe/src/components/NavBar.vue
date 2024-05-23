755d1ce76a3bedc8c2569274dc5167a5504377e5
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-05-23 14:36:00 +0700

    :lipstick: Add feedback btn on navbar to linkk to feedback section

--- my-portofolio-web/portofolio-fe/src/components/NavBar.vue
+++ my-portofolio-web/portofolio-fe/src/components/NavBar.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="sticky top-0 w-full bg-gradient-to-r from-sky-600 to-primary">
     <nav>
-      <div class="flex flex-row justify-end font-semibold text-light text-2xl px-6 py-3 md:justify-center md:text-lg">
+      <div class="flex flex-row justify-end font-semibold text-light text-2xl px-6 py-3 md:justify-center md:text-base">
         <a href="#" class="md:hidden">
           <i class="fa-solid fa-bars"></i>
         </a>
-        <ul class="sm:hidden md:flex md:flex-row md:space-x-8 ">
+        <ul class="sm:hidden md:flex md:items-center md:space-x-8 uppercase">
           <li><a href="#about-me">About Me</a></li>
           <li><a href="#history">History</a></li>
           <li><a href="#technology">Technology</a></li>
           <li><a href="#education">Education</a></li>
           <li><a href="#article">Article</a></li>
+          <li><a href="#feedback" class="inline-block rounded bg-light px-6 pb-2 pt-2.5 w-max leading-normal 
+                text-primary shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 
+                focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 
+                active:shadow-primary-2" data-twe-ripple-init data-twe-ripple-color="light">
+                <i class="fa-solid fa-reply"></i> Feedback
+          </a></li>
         </ul>
       </div>
     </nav>
